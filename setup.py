5cd64dc611f6d9e2debd4944ae5cd68905a68789
Matthias Baumgartner <baumgartner@ifi.uzh.ch>
2018-03-26 18:57:00 +0200

    distutils update

--- setup.py
+++ setup.py
@@ -1,12 +1,5 @@
 from distutils.core import setup
 
-pkg = ['hillie']
-
-pkg_data = {
-    'hillie': ['anedit', 'collected-words', 'gpop', 'hillie', 'hillie-o', 'pusher', 'stems.t', 'words.t']
-    }
-
-
 setup(
     name='hillie',
     version='1.0',
@@ -14,10 +7,13 @@ setup(
     long_description=open('README.md').read(),
     author='Matthias Baumgartner',
     author_email='dev@igsor.net',
-    url='http://www.igsor.net/projects/hillie/',
-    packages=pkg,
-    package_data = pkg_data,
-    scripts = ['anedit', 'gpop', 'hillie', 'hillie-o', 'pusher'],
+    url='https://github.com/igsor/hillie/',
+    packages=['hillie'],
+    package_data = {
+        '': ['README.md'],
+        'hillie': ['data/collected-words', 'data/stems.t', 'data/words.t']
+        },
+    scripts = ['anedit', 'hillie-p', 'hillie-o', 'pusher'],
     license='Free for use',
     requires=('lxml', 'stemming', 'levenshtein', 're', 'urllib', 'poppler', 'glib', 'magic', 'sqlite3')
 )
